6f6b106614f6827afc2afb287ac3be899e5c9770
tetuyoko <tyokoyama53@gmail.com>
2024-05-29 15:39:02 +0900

    feat: Add TestProj

---
+++ src/RResult.Test/UnitTest1.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RResult;
+
+namespace RResult.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestStartsWithUppder()
+    {
+        string[] words = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
+        foreach (var w in words)
+            Assert.IsTrue(StringLibrary.StartsWithUpper(w));
+    }
+
+    [TestMethod]
+    public void TestDoesNotStartWithUpper()
+    {
+        string[] words = { "alphabet", "zebra", "abc", "αυτοκινητοβιομηχανία", "государство",
+                               "1234", ".", ";", " " };
+        foreach (var w in words)
+            Assert.IsFalse(StringLibrary.StartsWithUpper(w));
+    }
+
+    [TestMethod]
+    public void DirectCallWithNullOrEmpty()
+    {
+        string?[] words = { string.Empty, null };
+        foreach (var w in words)
+            Assert.IsFalse(StringLibrary.StartsWithUpper(w));
+    }
+}
