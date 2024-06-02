9610eb26120972c5c071e5b4cebe572e60e9f840
tetuyoko <tyokoyama53@gmail.com>
2024-06-02 11:50:14 +0900

    refactor: Concat files

--- src/RResult/RResult.cs
+++ src/RResult/RResult.cs
@@ -1,6 +1,6 @@
 namespace RResult;
 
-public readonly partial record struct RResult<T, E> where E : notnull
+public readonly record struct RResult<T, E> where E : notnull
 {
     private readonly T? value;
     private readonly E? error;
@@ -63,4 +63,42 @@ public readonly partial record struct RResult<T, E> where E : notnull
             { IsOk: true } => onSuccess(value),
             _ => onFailure(error),
         };
+
+    // Returns result if the result is Ok, otherwise returns the Err value of self
+    public readonly RResult<T, E> And(RResult<T, E> result) =>
+        this switch
+        {
+            { IsOk: true } => result,
+            _ => this,
+        };
+
+    // Calls `onSuccess` function if the result is `Ok`
+    // `onSuccess` must always be of the same type as the receiver
+    public readonly RResult<T, E> AndThenBy(Func<T?, RResult<T, E>> onSuccess) =>
+        this switch
+        {
+            { IsOk: true } => onSuccess(value),
+            _ => this,
+        };
+
+    // Calls `onSuccess` function if the result is `Ok`
+    // Can transform  T => T2, but E must be same.
+    public readonly RResult<T2, E> AndThen<T2>(Func<T?, RResult<T2, E>> onSuccess) =>
+        this switch
+        {
+            { IsOk: true } => onSuccess(value),
+            _ => RResult<T2, E>.Err(error),
+        };
+
+    // Returns params `result` if the result is `Err`
+    public readonly RResult<T, E> Or(RResult<T, E> result) =>
+        this switch
+        {
+            { IsOk: true } => this,
+            _ => result,
+        };
+
+    // TODO: Add  `OrElse`
+
+    // TODO: Add  `OrElseBy`
 }
