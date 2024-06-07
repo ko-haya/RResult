b52e387a01b4edaf45b0601febd7f11e89caa409
tetuyoko <tyokoyama53@gmail.com>
2024-06-07 14:19:12 +0900

    feat: Add example api

--- example/RResult.Api/Program.cs
+++ example/RResult.Api/Program.cs
@@ -9,9 +9,14 @@ static IResult Test()
 {
     return User.FindUser(1)
                .AndThen(User.ValidateUser)
+               //.Map(User.AppendMeta)
+               //.AndThen(User.UpdateDb)
+               //.Inspect(user => PutLog(""))
+               //.AndThen(User.WriteMail)
+               //.AndThen(User.SendMail)
                .MapBoth<IResult>
                (
-                   Ok => TypedResults.Ok($"Hello! {Ok.Name}"),
+                   Ok => TypedResults.Ok($"Hello! {Ok.Name}({Ok.Id})"),
                    Err => TypedResults.NotFound($"Error: {Err}")
                );
 }
--- example/RResult.Api/User.cs
+++ example/RResult.Api/User.cs
@@ -1,19 +1,26 @@
 using RResult;
 namespace myAPI;
 
-public record struct User(int Id, string Name)
+public record struct User(int Id, string Name, string? Meta)
 {
-    public static RResult<User, string> FindUser(int id) =>
-        RResult<User, string>.Ok(new(1, "hoge"));
+    // shorthand
+    public static RResult<User, string> Ok(User user) =>
+        RResult<User, string>.Ok(user);
+    public static RResult<User, string> Err(string message) =>
+        RResult<User, string>.Err(message);
 
-    public static RResult<User, string> FindErr(int id) =>
-        RResult<User, string>.Err($"Not found: {id}");
+    public static RResult<User, string> FindUser(int id, bool success = true) =>
+        success switch
+        {
+            true => Ok(new(1, "hoge", default)),
+            _ => Err($"Not found: {id}") // TODO: Enum
+        };
 
     public static RResult<User, string> ValidateUser(User user) =>
         user switch
         {
-            { Id: 1 } => RResult<User, string>.Ok(user),
-            _ => RResult<User, string>.Err($"Not valid: {user.Id}") // TODO: Enum
+            { Id: 1 } => Ok(user),
+            _ => Err($"Not valid: {user.Id}") // TODO: Enum
         };
 
 
