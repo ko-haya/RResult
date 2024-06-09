fb9ee93d4d15d393a4bafeef26f5937b1f4ae5ff
tetuyoko <tyokoyama53@gmail.com>
2024-06-09 13:29:02 +0900

    feat(sample): Use enum included record to handle error

--- example/RResult.Api/Program.cs
+++ example/RResult.Api/Program.cs
@@ -12,13 +12,19 @@ app.Run();
 
 static async Task<IResult> Get(int id = 1) =>
     await User.Find(id)
-        .Map(User.AppendMeta)
-        .AndThen(User.Validate)
-        .AndThenAsync(User.Update)
-        .AndThenAsync(User.WriteMail)
-        .InspectAsync(it => Console.WriteLine($"PutLog: {it.Text}"))
-        .AndThenAsync(User.SendMail)
-        .MapBothAsync<User, string, IResult>(
-            Ok => TypedResults.Ok($"Hello! {Ok.Name}({Ok.Id})[{Ok.Meta}]"),
-            Err => TypedResults.NotFound($"Error: {Err}")
-        );
+          .Map(User.AppendMeta)
+          .AndThen(User.Validate)
+          .AndThenAsync(User.Update)
+          .AndThenAsync(User.WriteMail)
+          .InspectAsync(it => Console.WriteLine($"PutLog: {it.Text}"))
+          .AndThenAsync(User.SendMail)
+          .MapBothAsync<User, ErrT, IResult>(
+              Ok => TypedResults.Ok($"Hello! {Ok.Name}({Ok.Id})[{Ok.Meta}]"),
+              Err => Err switch
+              {
+                  { apiErr: ApiErr.NotFound } => TypedResults.NotFound(Err.desc),
+                  { apiErr: ApiErr.Unknown } => TypedResults.UnprocessableEntity(Err.desc),
+                  { apiErr: ApiErr.Hoge } => TypedResults.BadRequest(Err.desc),
+                  _ => TypedResults.NotFound($"Error: {Err.desc}")
+              }
+          );
