e1441ddb796f9c01a75eba5612710774f127ba8f
tetuyoko <tyokoyama53@gmail.com>
2024-06-24 12:27:41 +0900

    Add OrElseAsync tests

--- src/RResult.Test/OrTest.cs
+++ src/RResult.Test/OrTest.cs
@@ -15,10 +15,10 @@ public partial class OrTest
     public void TestOrElse()
     {
         static RResult<int, int> LSq(int x) => RResult<int, int>.Ok(x * x);
-        static RResult<int, int> LErr(int x) => RResult<int, int>.Err(x);
-        Assert.AreEqual(LSq(2).OrElse(LSq).OrElse(LSq).Unwrap, 4);
-        Assert.AreEqual(LSq(2).OrElse(LErr).OrElse(LSq).Unwrap, 4);
-        Assert.AreEqual(LErr(3).OrElse(LSq).OrElse(LErr).Unwrap, 9);
-        Assert.AreEqual(LErr(3).OrElse(LErr).OrElse(LErr).UnwrapErr, 3);
+        static RResult<int, int> LErr(int x) => RResult<int, int>.Err(x - 1);
+        Assert.AreEqual(LSq(2).OrElse(LSq).OrElse(LSq).ToString(), "Ok(4)");
+        Assert.AreEqual(LSq(2).OrElse(LErr).OrElse(LSq).ToString(), "Ok(4)");
+        Assert.AreEqual(LErr(3).OrElse(LSq).OrElse(LErr).ToString(), "Ok(4)");
+        Assert.AreEqual(LErr(3).OrElse(LErr).OrElse(LErr).ToString(), "Ok(0)");
     }
 }
