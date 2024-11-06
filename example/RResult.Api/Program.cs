cee096e26d70a8e2160b0f808e60f3a32de3c52e
tetuyoko <tyokoyama53@gmail.com>
2024-11-06 17:46:00 +0900

    feat: Live with OpenAPI

--- example/RResult.Api/Program.cs
+++ example/RResult.Api/Program.cs
@@ -1,29 +1,67 @@
+using Microsoft.EntityFrameworkCore;
+
 using RResult.Api;
 using RResult;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", Get);
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapGet("/favicon.ico", () => TypedResults.NotFound());
+
+app.MapGet("/", () => Get(1));
 app.MapGet("/{id}", Get);
+
+string[] Summaries =
+["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+
+app.MapGet("/weather", Results<Ok<WeatherForecast>, NotFound> () =>
+    {
+        return TypedResults.Ok(new WeatherForecast(
+            DateTime.Now.AddDays(2),
+            Random.Shared.Next(-20, 55),
+            Summaries[Random.Shared.Next(Summaries.Length)]
+        ));
+    });
+
+// Todos
+app.MapGet("/todoitems", async (TodoDb db) =>
+    await db.Todos.ToListAsync());
 
 app.Run();
 
-static async Task<IResult> Get(int id = 1) =>
+
+static async Task<Results<Ok<User>, NotFound<string>, UnprocessableEntity<string>, BadRequest<string>>> Get(int id = 1) =>
     await User.Find(id)
           .Map(User.AppendMeta)
           .AndThen(User.Validate)
           .AndThenAsync(User.Update)
           .AndThenAsync(User.WriteMail)
           .InspectAsync(it => Console.WriteLine($"PutLog: {it.Text}"))
-          .AndThenAsync(User.SendMail)
-          .MapBothAsync<User, ErrT, IResult>(
-              Ok => TypedResults.Ok($"Hello! {Ok.Name}({Ok.Id})[{Ok.Meta}]"),
-              Err => Err switch
-              {
-                  { apiErr: ApiErr.NotFound } => TypedResults.NotFound(Err.desc),
-                  { apiErr: ApiErr.Unknown } => TypedResults.UnprocessableEntity(Err.desc),
-                  { apiErr: ApiErr.Hoge } => TypedResults.BadRequest(Err.desc),
-                  _ => TypedResults.NotFound($"Error: {Err.desc}")
-              }
-          );
+          .AndThenAsync(User.SendMail) switch
+    {
+        { IsOk: true, Unwrap: var ok } => TypedResults.Ok(ok),
+        { IsOk: false, UnwrapErr: var err }
+            when err.apiErr is ApiErr.NotFound => TypedResults.NotFound(err.desc),
+        { IsOk: false, UnwrapErr: var err }
+            when err.apiErr is ApiErr.Unknown => TypedResults.UnprocessableEntity(err.desc),
+        { IsOk: false, UnwrapErr: var err } => TypedResults.BadRequest(err.desc),
+    };
+
+record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
